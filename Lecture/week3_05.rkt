99a3c017fc8ae5565ba5c6fcf92caf57db0cd718
kimyouyoung <21800147@handong.edu>
2020-09-18 14:17:24 +0900

    updated

---
+++ Lecture/week3_05.rkt
@@ -0,0 +1,88 @@
+#lang plai
+; interpreter => AE -> number
+(define-type AE
+  [num (n number?)]
+  [add (lhs AE?)
+       (rhs AE?)]
+  [sub (lhs AE?)
+       (rhs AE?)])
+
+; '{+ {- 2 1} 3} ==> concrete syntax
+; ae1's type is 'add'
+; add-rhs -> '-' = in java '.'
+(define ae1 (add (sub (num 3)(num 4)) (num 7))) ; ==> abstract syntax
+(sub? ae1)
+(add-lhs ae1)
+(add-rhs ae1)
+
+; parse: sexp -> AE
+; to convert s-expressions into AEs in abstract syntax
+(define (parse sexp)
+  (cond
+    [(number? sexp)(num sexp)]
+    [(and (= 3 (length sexp))
+          (eq? (first sexp) '+))
+     (add(parse(second sexp))
+         (parse(third sexp)))]
+    [(and (= 3 (length sexp))
+          (eq? (first sexp) '-))
+     (sub(parse(second sexp))
+         (parse(third sexp)))]
+    [else (error 'parse "bad syntax: ~a" sexp)]
+    ))
+
+; tests
+(test(parse '3)(num 3))
+(test(parse '{+ 3 4})(add (num 3)(num 4)))
+(test(parse '{+ {- 5 4} 5})(add(sub (num 5)(num 4))(num 5)))
+(test/exn(parse '{-5 1 2})"parse: bad syntax: (-5 1 2)")
+       
+;(parse '{+ 3 4})
+;(parse '{+ 3 4 5})
+
+(define (interp ae)
+  (type-case AE ae
+    ; n is recognized as actual number for computers.
+    [num (n) n]
+    ; add is recognized as a real behavior to sum two AEs. 
+    [add (l r) (+(interp l)(interp r))]
+    ; sub is recognized as a real behavior to substract two AEs.
+    [sub (l r) (-(interp l)(interp r))]))
+
+(interp (parse '3))
+(interp (parse '{+ 1 2}))
+(interp (parse '{+{- 1 2} 5}))
+
+;infix parse
+(define (parse-infix sexp)
+  (cond
+    [(number? sexp)(num sexp)]
+    [(and (= 3 (length sexp))
+          (eq? (second sexp) '+))
+     (add(parse-infix(first sexp))
+         (parse-infix(third sexp)))]
+    [(and (= 3 (length sexp))
+          (eq? (second sexp) '-))
+     (sub(parse-infix(first sexp))
+         (parse-infix(third sexp)))]
+    [else (error parse-infix "bad syntax: ~a" sexp)]
+    ))
+
+(interp (parse-infix '{2 + {9 - 2}}))
+
+;postfix parse
+(define (parse-post sexp)
+  (cond
+    [(number? sexp)(num sexp)]
+    [(and (= 3 (length sexp))
+          (eq? (third sexp) '+))
+     (add(parse-post(first sexp))
+         (parse-post(second sexp)))]
+    [(and (= 3 (length sexp))
+          (eq? (third sexp) '-))
+     (sub(parse-post(first sexp))
+         (parse-post(second sexp)))]
+    [else (error parse-post "bad syntax: ~a" sexp)]
+    ))
+
+(interp (parse-post '{2{9 2 -}+}))
---
+++ Lecture/week3_06.rkt
@@ -0,0 +1,28 @@
+#lang plai
+
+;{+{-{+ 5 5}3}{-{+ 5 5}3}}
+;{with{x {+ 5 5}}{with{y {- x 3}}{+ y y}}}
+
+(define-type WAE
+  [num (n number?)]
+  [add (lhs WAE?)(rhs WAE?)]
+  [sub (lhs WAE?)(rhs WAE?)]
+  [with (name symbol?)(named-expr WAE?)(body WAE?)]
+  [id (name symbol?)])
+
+;[contract] parse: sexp -> WAE
+;[purpose] to convert s-expression into WAE
+(define (parse sexp)
+  (match sexp
+    [(? number?) (num sexp)]
+    [(list '+ l r) (add (parse l)(parse r))]
+    [(list '- l r) (sub (parse l)(parse r))]
+    [(list 'with (list i v) e) (with i (parse v)(parse e))]
+    [(? symbol?) (id sexp)]
+    [else (error 'parse "bad syntax:~a"sexp)]))
+
+(test(parse '{+{- 3 4} 7})(add(sub (num 3)(num 4))(num 7)))
+(test(parse '{with{x 5}{+ 8 2}})(with 'x(num 5)(add(num 8)(num 2))))
+(test(parse '{with{x 5}{+ x x}})(with 'x(num 5)(add(id 'x)(id 'x))))
+
+(parse '{with {x {+ 5 5}}{+ x x}})
